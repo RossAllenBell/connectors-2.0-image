cbc7371e17a85628a0917a2e9dac21b9964dc7fe
Ross Bell <rossallenbell@gmail.com>
2021-12-28 16:59:33 -0600

    Make connector capable of indexing documents in response to a request

--- connector.rb
+++ connector.rb
@@ -9,6 +9,8 @@ class Connector < Sinatra::Base
   use ElasticAPM::Middleware
   use EcsLogging::Middleware, $stdout
 
+  SOURCE_NAME = 'Test Custom Source Ruby'
+
   configure do
     set :environment, :production
     set :port, 80
@@ -29,6 +31,23 @@ class Connector < Sinatra::Base
 
     client = Elastic::EnterpriseSearch::WorkplaceSearch::Client.new(host: host, http_auth: http_auth)
 
-    client.list_content_sources.to_json
+    content_source_id = client.list_content_sources.body.fetch('results').detect do |source|
+      source.fetch('name') == SOURCE_NAME
+    end&.fetch('id')
+
+    if content_source_id.nil?
+      content_source_id = client.create_content_source(name: SOURCE_NAME).body.fetch('id')
+    end
+
+    documents = [
+      { id: 'e68fbc2688f1', title: 'Frankenstein; Or, The Modern Prometheus', author: 'Mary Wollstonecraft Shelley' },
+      { id: '0682bb06af1a', title: 'Jungle Tales', author: 'Horacio Quiroga' },
+      { id: '75015d85370d', title: 'Lenguas de diamante', author: 'Juana de Ibarbourou' },
+      { id: 'c535e226aee3', title: 'Metamorphosis', author: 'Franz Kafka' },
+    ]
+
+    client.index_documents(content_source_id, documents: documents)
+
+    'Consider it done.'
   end
 end
